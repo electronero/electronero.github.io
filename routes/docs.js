bf473c624b26d63d3fdb12d89f0f5762453c3399
Interchained <president@worldvaporexpo.com>
2018-05-07 05:14:45 -0400

    Update docs.js

--- routes/docs.js
+++ routes/docs.js
@@ -9,8 +9,12 @@ router.get('/', function(req, res, next) {
 router.get('/wallet-rpc-documentation', function(req, res, next) {
   res.render('wallet-rpc-documentation', { title: 'Electronero Wallet RPC Documentation' });
 });
-// get daemon-rpc-documentation
+// get daemon-rpc-documentation 
 router.get('/daemon-rpc-documentation', function(req, res, next) {
   res.render('daemon-rpc-documentation', { title: 'Electronero Daemon RPC Documentation' });
+});
+// get offline-backup
+router.get('/offline-backup', function(req, res, next) {
+  res.render('offline-backup', { title: 'Electronero Offline Backup' });
 });
 module.exports = router;
