b86f219ec44e958527740cf672a019fcd6bf4964
Interchained <president@worldvaporexpo.com>
2018-05-19 21:20:17 -0400

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -5,4 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('blog', { title: 'Electronero Blog' });
 });
+router.get('/initializing-testnet-may-03-2018', function(req, res, next) {
+  res.render('may-03-2018', { title: 'Electronero Blog | May 03, 2018' });
+});
+router.get('/initializing-testnet-may-03-2018', function(req, res, next) {
+  res.render('may-07-2018', { title: 'Electronero Blog | May 07, 2018' });
+});
 module.exports = router;
