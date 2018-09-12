424fdfe480682bef7115ad3537ef8821723eacc2
Interchained <president@worldvaporexpo.com>
2018-09-12 18:46:25 -0400

    update meta data for SEO proficiency

--- routes/hardfork.js
+++ routes/hardfork.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
    /*ROUTE FORMAT*/
    /*res.render('EJSTEMPLATENAM', { title: 'METATDATA' });*/
-  res.render('hardfork', { title: 'Hash Factory: Upcoming Hard Fork' });
+  res.render('hardfork', { title: 'Electronero: Upcoming Hard Fork' });
 });
 
 module.exports = router;
